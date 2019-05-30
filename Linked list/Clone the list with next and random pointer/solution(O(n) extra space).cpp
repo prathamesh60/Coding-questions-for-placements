9b7830bc56a4179f1d7f695465e975532ef9de31
prathamesh60 <41123704+prathamesh60@users.noreply.github.com>
2019-05-30 22:42:02 +0530

    Create solution(O(n) extra space).cpp

---
+++ Linked list/Clone the list with next and random pointer/solution(O(n) extra space).cpp
@@ -0,0 +1,49 @@
+/*This is a function problem.You only need to complete the function given below*/
+/*  the node structure is as follows
+struct Node {
+    int data;
+    Node *next;
+    Node *arb;
+    Node(int x) {
+        data = x;
+        next = NULL;
+        arb = NULL;
+    }
+};
+*/
+// Should return the head of the copied linked list the
+// output will be 1 if successfully copied
+Node *newnode(int data)
+{ Node *nptr=(Node*)malloc(sizeof(Node));
+  nptr->next=NULL;
+  nptr->arb=NULL;
+  nptr->data=data;
+  return nptr;
+}
+Node *copyList(Node *head) {
+     map<Node*,Node*> m;
+     Node *curr=head;
+     Node *copy=NULL,*kptr;
+    // m[NULL]=NULL;
+     while(curr!=NULL)
+     {
+        kptr=newnode(curr->data);
+        m[curr]=kptr;
+        curr=curr->next;
+     }
+     curr=head;
+    // copy=m[curr];
+     while(curr!=NULL)
+     {
+         copy=m[curr];
+         if(curr->next!=NULL)
+          copy->next=m[curr->next];
+         else
+          copy->next=NULL;
+         
+         copy->arb=m[curr->arb];
+         curr=curr->next;
+     }
+     return m[head];
+     
+}
